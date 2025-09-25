27ede61331e78076ae013f574f82494cb2292a31
Your Name <you@example.com>
2025-09-25 21:13:02 +0530

    ternary added

--- app/admin/addproduct/page.jsx
+++ app/admin/addproduct/page.jsx
@@ -11,7 +11,7 @@ const page = () => {
         <form className='pt-5 px-5 sm:pt-12 sm:pl-16'>
             <p className='text-xl'>Upload Thumbnail</p>
             <label htmlFor='image' >
-            <Image className='mt-4 cursor-pointer' src={assets.upload_area} alt='' width={140} height={70}  />
+            <Image className='mt-4 cursor-pointer' src={!image ? assets.upload_area:URL.createObjectURL(image)} alt='' width={140} height={70}  />
             </label>
             
             <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='image' hidden required />
